94068b8d7d36e973b60f4ee9082da4cd5e7f4b25
timosarkar <timosarkartimo@gmail.com>
2025-04-07 20:43:54 +0200

    Create shell.nix

---
+++ bug-bounty/shell.nix
@@ -0,0 +1,21 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.subfinder # subdomain enumeration
+    #pkgs.naabu # superfast port scanner
+    pkgs.httpx # HTTP probing tool
+    pkgs.katana # web application scanner
+    #pkgs.gau # Get all URLs from JS files
+    #pkgs.ffuf # fuzzer
+    #pkgs.inql # graphql injection tool
+    #pkgs.sqlmap # SQL injection tool
+    pkgs.dalfox # XSS scanner
+    pkgs.nuclei # vulnerability scanner
+  ];
+
+  shellHook = ''
+  # find xss: subfinder -d <domain> | httpx | katana | dalfox pipe
+  # find vuln: subfinder -d <domain> | httpx | katana | nuclei -t <template>
+  '';
+}
